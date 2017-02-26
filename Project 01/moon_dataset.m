fde0d3f65aa7e47f0c032757a7dccebcc67d79eb
Saeid Dadkhah <saeid.dadkhah@live.com>
2017-02-27 01:43:38 +0330

    moon dataset generator is working fine.

--- Project 01/moon_dataset.m
+++ Project 01/moon_dataset.m
@@ -4,11 +4,11 @@ r_rand = r - w/2 + w*rand(num, 1);
 theta_rand = pi*rand(num, 1);
 X = [r_rand.*cos(theta_rand), r_rand.*sin(theta_rand)];
 
-y = ones(num, 1);
+y = -1*ones(num, 1);
 
 r_rand = r - w/2 + w*rand(num, 1);
 theta_rand = -pi*rand(num, 1);
 X = [X; ...
     r + r_rand.*cos(theta_rand), -d + r_rand.*sin(theta_rand)];
 y = [y; ...
-    2*ones(num, 1)];
+    ones(num, 1)];
