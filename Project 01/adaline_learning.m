979c31f20c745bbe7a639a3e323db8a1e6fc0879
Saeid Dadkhah <saeid.dadkhah@live.com>
2017-03-07 21:55:02 +0330

    learning rate is added to arguments

--- Project 01/adaline_learning.m
+++ Project 01/adaline_learning.m
@@ -1,9 +1,7 @@
-function w = adaline_learning(X, y, w_init)
-
-alpha = 0.001;
+function w = adaline_learning(X, y, w_init, learning_rate)
 
 w = w_init;
 
 for i = 1:(size(X, 1))
-    w = w + alpha * (y(i) - X(i, :)*w) * X(i, :)';
+    w = w + learning_rate* (y(i) - X(i, :)*w) * X(i, :)';
 end
--- Project 01/perceptron_learning.m
+++ Project 01/perceptron_learning.m
@@ -1,9 +1,17 @@
-function w = perceptron_learning(X, y, w_init)
+function w = perceptron_learning(X, y, w_init, learning_rate)
 
 w = w_init;
 
-for i = 1:size(X, 1)
-    alpha = 1 / sqrt((X(i, :)*X(i, :)'));
-    y_hat =sign(X(i, :) * w);
-    w = w + alpha .* (y(i) - y_hat) * X(i, :)';
+if learning_rate < 0
+    for i = 1:size(X, 1)
+        alpha = 1 / sqrt((X(i, :)*X(i, :)'));
+        y_hat =sign(X(i, :) * w);
+        w = w + alpha .* (y(i) - y_hat) * X(i, :)';
+    end
+else
+    for i = 1:size(X, 1)
+        %alpha = 1 / sqrt((X(i, :)*X(i, :)'));
+        y_hat =sign(X(i, :) * w);
+        w = w + learning_rate .* (y(i) - y_hat) * X(i, :)';
+    end
 end
